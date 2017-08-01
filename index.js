198bcb9571b53b8d213216c667baabb57e269f13
doowb <brian.woodward@gmail.com>
2017-08-01 11:45:42 -0400

    implementation
     - bring in code from file-normalize (original introduced in PR https://github.com/jonschlinkert/file-normalize/pull/3)
     - add tests

--- index.js
+++ index.js
@@ -1,4 +1,35 @@
 'use strict';
 
-module.exports = function() {
+var os = require('os');
+
+/**
+ * Append a string to another string ensuring to preserve line ending characters.
+ *
+ * ```js
+ * console.log([appendString('abc\r\n', 'def')]);
+ * //=> [ 'abc\r\ndef\r\n' ]
+ *
+ * console.log([appendString('abc\n', 'def')]);
+ * //=> [ 'abc\ndef\n' ]
+ *
+ * // uses os.EOL when a line ending is not found
+ * console.log([appendString('abc', 'def')]);
+ * //=> [ 'abc\ndef' ]
+ * ```
+ * @param  {String} `str` String that will be used to check for an existing line ending. The suffix is appended to this.
+ * @param  {String} `suffix` String that will be appended to the str.
+ * @return {String} Final String
+ * @api public
+ */
+
+module.exports = function appendString(str, suffix) {
+  var eol;
+  if (str.slice(-2) === '\r\n') {
+    eol = '\r\n';
+  } else if (str.slice(-1) === '\n') {
+    eol = '\n';
+  } else {
+    return [str, os.EOL, suffix].join('');
+  }
+  return [str, suffix, eol].join('');
 };
---
+++ test.js
@@ -0,0 +1,32 @@
+'use strict';
+
+require('mocha');
+var os = require('os');
+var assert = require('assert');
+var appendString = require('./');
+
+describe('appendString', function() {
+  it('should append the string suffix to the string prefix with \\r\\n', function() {
+    assert.equal(appendString('abc\r\n', 'def'), 'abc\r\ndef\r\n');
+  });
+
+  it('should append the string suffix to the string prefix with \\n:', function() {
+    assert.equal(appendString('abc\n', 'def'), 'abc\ndef\n');
+  });
+
+  it('should append the string suffix to the string prefix without EOL', function() {
+    assert.equal(appendString('abc', 'def'), 'abc' + os.EOL + 'def');
+  });
+
+  it('should append the buffer suffix to the string prefix with \\r\\n', function() {
+    assert.equal(appendString('abc\r\n', new Buffer('def')), 'abc\r\ndef\r\n');
+  });
+
+  it('should append the buffer suffix to the string prefix with \\n:', function() {
+    assert.equal(appendString('abc\n', new Buffer('def')), 'abc\ndef\n');
+  });
+
+  it('should append the buffer suffix to the string prefix without EOL', function() {
+    assert.equal(appendString('abc', new Buffer('def')), 'abc' + os.EOL + 'def');
+  });
+});
